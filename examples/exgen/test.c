d1648d8231bb5fa4c9f97594430f187ef896af42
Zzzode <Zzzode@github.com>
2020-04-16 18:59:35 +0800

    gitignore and executor

---
+++ examples/exgen/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int plus(int a, int b){
+    int c;
+    result = a + b;
+    return c;
+}
+
+int test(int x, int y){
+    int result;
+    puts("test");
+    result = plus(x, y);
+    return result;
+}
+
+int main(){
+    int a = 1, b = 2;
+    test(a, b);
+    return 0;
+}
