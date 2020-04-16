cdcae75e1523fb1fe38349f6fb38d816ddaaf91c
Zzzode <Zzzode@github.com>
2020-04-16 19:00:16 +0800

    manully generate testcase

--- examples/exgen/test.c
+++ examples/exgen/test.c
@@ -1,20 +1,33 @@
-#include <stdio.h>
+#include "klee/klee.h"
 
 int plus(int a, int b){
     int c;
-    result = a + b;
+    c = a + b;
     return c;
 }
 
 int test(int x, int y){
     int result;
-    puts("test");
     result = plus(x, y);
     return result;
 }
 
 int main(){
-    int a = 1, b = 2;
-    test(a, b);
+    int a, b, c = -1;
+    int result;
+    klee_make_symbolic(&a, sizeof(a), "a");
+    klee_make_symbolic(&b, sizeof(b), "b");
+    klee_make_symbolic(&c, sizeof(c), "c");
+    klee_make_symbolic(&result, sizeof(result), "result");
+    // klee_assume(result == 1 & a > 0 & b > 0);
+    if(a > 0 && b > 0){
+        c = test(a, b);
+        if(c < a)
+            result = 1;
+        else
+            result = 0;
+    } else
+        result = 2;
+    
     return 0;
 }
